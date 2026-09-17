bd978f72c5963f96eb02a5cd907c390b526b2eb5
Tiphe0821 <namucho24@gmail.com>
2026-09-17 15:47:14 +0900

    [Feat] 복셀 캐릭터와 아레나

--- 2609_Introduction_3d/Assets/Scripts/PlayerController.cs
+++ 2609_Introduction_3d/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,6 +15,15 @@ public class PlayerController : MonoBehaviour
     private float mouseSensitivity = 0.2f;
 
     private float verticalVelocity;
+
+    public Transform cameraPivot;
+    public Transform cameraTransform;
+
+
+    private float pitch = 20f;
+
+    private bool isRunning;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -39,10 +49,17 @@ public class PlayerController : MonoBehaviour
         lookInput = value.Get<Vector2>();
     }
 
+    private void OnSprint(InputValue value)
+    {
+        isRunning = value.isPressed;
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0f, lookInput.x * mouseSensitivity, 0f);
+
+
 
         if(controller.isGrounded && verticalVelocity < 0.0f)
         {
@@ -54,9 +71,26 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
 
         //Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        move = move * moveSpeed;
+        float speed = moveSpeed;
+        float targetZ = -3f;
+        if (isRunning)
+        {
+            speed = moveSpeed * 2f;
+            targetZ = -5f;
+        }
+        move = move * speed;
+
+        Vector3 camPos = cameraTransform.localPosition;
+        camPos.z = Mathf.Lerp(camPos.z, targetZ, 5f * Time.deltaTime);
+        cameraTransform.localPosition = camPos;
+
+
         move.y = verticalVelocity;
 
         controller.Move(move* Time.deltaTime);
+
+        pitch = pitch - lookInput.y * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, -20f, 60f);
+        cameraPivot.localEulerAngles = new Vector3(pitch, 0f, 0f);
     }
 }
